256b8dde73b5712c928de563e9bd052f1e6b1808
markusregobenedek-blip <markus.rego.benedek@szbiszeged.hu>
2025-11-06 14:31:16 +0100

    feltoltve a kedvezmenyek es az eletkor(kortábla)

--- backend/public/schedules/js/adatok.mjs
+++ backend/public/schedules/js/adatok.mjs
@@ -6,10 +6,28 @@ let jarat = ['busz', 'vonat'];
 let allomasok = ['Budapest', 'Győr', 'Pécs', 'Szeged', 'Debrecen', 'Miskolc'];
 
 // Kedvezmények (%)
-let kedvezmenyek = ['0', '25', '50', '90'];
+let kedvezmenyek = [' BKK bérletek és jegyek (csak HÉV utazáshoz)', 'BKK munkavállaló 2. kocsiosztály', 'BKV munkavállaló 2. kocsiosztály', 'Ellátottak utazási utalványa','Ellátottak utazási utalványa', 'FIP igazolvány 1. oszt.', 'Fogyatékossággal élők igazolványa', 'GYSEV igazolvány 1. oszt.', 'GYSEV igazolvány 2. oszt.', 'GYSEV igazolvány-családtag 1. oszt.', 'GYSEV igazolvány-családtag 2. oszt.  Díjmentes','Magyar Igazolvány, Magyar hozzátartozói Igazolvány  Díjmentes','Magyarország24 jegy  Díjmentes','MÁV-START igazolvány 1. oszt.  Díjmentes',
+  'MÁV-START igazolvány 2. oszt.  Díjmentes',
+  'MÁV-START igazolvány-családtag 1. oszt.  Díjmentes',
+  'MÁV-START igazolvány-családtag 2. oszt.  Díjmentes',
+  'Menekültek igazolása  Díjmentes',
+  'Nagcsalád tagja  Díjmentes',
+  'Nemzetközi bérlet, menetjegy 1. o.  Díjmentes',
+  'Nemzetközi bérlet, menetjegy 2. o.  Díjmentes',
+  'Országbérlet  ',
+  'OSZZSD igazolvány magáncélú  Díjmentes',
+  'Rendőr - készenléti igazolvány (203)  Díjmentes',
+  'START Klub VIP kártya  Díjmentes',
+  'U jelű, vagy azzal egyenértékű utazási igazolvány  Díjmentes',
+  'VOLÁN szabadjegy (országos)  Díjmentes'];
 
 // Kortábla minták
-let kortablak = ['A', 'B', 'C', 'D'];
+let kortablak = ['Kisgyerek (0-3 éves)',
+  'Kisgyerek (3-6 éves)',
+  'Gyerek (6-14 éves)',
+  'Gyerek [14-18 éves]',
+  'Felnőtt [25-65 éves]',
+  'Nyugdíjas/senior (65+ éves)'];
 
 // Időtartamok (példa)
 let idotartamok = ['30 perc', '1 óra', '1 óra 30 perc', '2 óra', '2 óra 30 perc'];
