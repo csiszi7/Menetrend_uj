69401d464b4a75325b818072426561870610d084
ig:csiszi.06 <sinko.szilard@szbiszeged.hu>
2025-11-10 12:49:44 +0100

    adat berakas

--- backend/public/schedules/js/schedule-modosit.mjs
+++ backend/public/schedules/js/schedule-modosit.mjs
@@ -1,12 +1,17 @@
 async function modosit(event, id) {
     event.preventDefault();
     const jarat = document.querySelector('#jarat').value;
-    const nap = document.querySelector('#nap').value;
-    const kedvezmeny = document.querySelector('#kedvezmeny').value;
-    const indulasiido = document.querySelector('#indulasiido').value;
-    const erkezesiido = document.querySelector('#erkezesiido').value;
-    const ar = document.querySelector('#ar').value;
-
+    const idotartamok = document.querySelector('#idotartamok').value;
+    const kedvezmenyek = document.querySelector('#kedvezmenyek').value;
+    const kortablak = document.querySelector('#kortablak').value;
+    const allomasok = document.querySelector('#allomasok').value;
+    const idopontok = document.querySelector('#idopontok').value;
+    const induloallomas = document.querySelector('#induloallomas').value;
+    const celallomas = document.querySelector('#celallomas').value;
+    const indulasideje = document.querySelector('#indulasideje').value;
+    const visszaideje = document.querySelector('#visszaideje').value;
+    const helyjegy = document.querySelector('#helyjegy').value;
+    const klima = document.querySelector('#klima').value;
     const response = await fetch(`/api/schedules-backend/${id}`, {
         method: 'PUT',
         headers: {
@@ -15,10 +20,20 @@ async function modosit(event, id) {
         body: JSON.stringify({
             jarat,
             nap,
-            kedvezmeny,
+            kedvezmenyek,
             ar,
             indulasiido,
             erkezesiido,
+            idotartamok,
+            kortablak,
+            allomasok,
+            idopontok,
+            induloallomas,
+            celallomas,
+            indulasideje,
+            visszaideje,
+            helyjegy,
+            klima
         }),
     });
 
