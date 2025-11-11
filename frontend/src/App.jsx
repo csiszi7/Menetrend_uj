50ee8df482ac75f210f948636bf364a38f625d86
ig:csiszi.06 <sinko.szilard@szbiszeged.hu>
2025-11-11 12:25:41 +0100

    frontend tartalmai is belekerultek mostmar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Navbar from './components/Navbar/Navbar';
+import Home from "./pages/Home";
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Jegy from "./components/Jegy";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Menetrend from "./components/menetrend";
 
 function App() {
-    return (
-        <div className="app-kontener">
-            <BrowserRouter>
-                <Navbar />
-                <Routes>
-                    <Route
-                        path="/"
-                        element={<Home />}
-                    />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
-}
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/jegy" element={<Jegy />} />
+        <Route path="/menetrend" element={<Menetrend />} />
+      </Routes>
+    </BrowserRouter>
+  )
+} 
 
 export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Jegy from '../components/Jegy';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-kontener">
+        <h1>Menetrend</h1>
+    </div>
+  )
+}
+
+export default Home;
