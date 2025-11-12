e6ed3a54e285fa5a43eaa3e80433e5b3325bdf7e
ig:csiszi.06 <sinko.szilard@szbiszeged.hu>
2025-11-12 15:09:54 +0100

    module problema

--- backend/public/schedules/js/menetrendek.mjs
+++ backend/public/schedules/js/menetrendek.mjs
@@ -1,53 +1,75 @@
-async function feltoltes(event) {
-    event.preventDefault();
+let feltoltesGomb = document.querySelector("#feltoltes");
+feltoltesGomb.addEventListener("click", () => feltoltes(event)); 
 
-    try {
-        // Alap mezők beolvasása
-        const jarat = document.querySelector("#jarat").value;
-        const induloallomas = document.querySelector("#induloallomas").value;
-        const celallomas = document.querySelector("#celallomas").value;
+// async function feltoltes(event) {
+//     event.preventDefault();
 
-        // Többes kijelölés beolvasása
-        const allomasokSelect = document.querySelector("#allomasok");
-        const allomasok = Array.from(allomasokSelect.selectedOptions).map(o => o.value);
+//     try {
+//         // Alap mezők beolvasása
+//         const jarat = document.querySelector("#jarat").value;
+//         const induloallomas = document.querySelector("#induloallomas").value;
+//         const celallomas = document.querySelector("#celallomas").value;
 
-        const idopontok = document.querySelector("#idopontok").value.split(","); // ha vesszővel választod el
-        const kedvezmeny = document.querySelector("#kedvezmeny").value;
-        const klima = document.querySelector("#klima").checked; // ha checkbox/radio
-        const helyjegy = document.querySelector("#helyjegy").checked;
+//         // Többes kijelölés beolvasása
+//         const allomasokSelect = document.querySelector("#allomasok");
+//         const allomasok = Array.from(allomasokSelect.selectedOptions).map(o => o.value);
 
-        const kep1 = document.querySelector("#kep1").value;
-        const kep2 = document.querySelector("#kep2").value;
-        const kep3 = document.querySelector("#kep3").value;
-        const kepek = [kep1, kep2, kep3];
+//         const kedvezmeny = document.querySelector("#kedvezmeny").value;
+//         const kortablak = document.querySelector("#kortablak").value;
+//         const idotartam = document.querySelector("#idotartam").value;
+//         const idopontok = document.querySelector("#idopontok").value.split(","); // ha vesszővel választod el
+//         const klima = document.querySelector("#klima").checked; // ha checkbox/radio
+//         const helyjegy = document.querySelector("#helyjegy").checked;
 
-        // Küldés backend felé
-        const response = await fetch(`/api/new-schedule`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                jarat,
-                induloallomas,
-                celallomas,
-                allomasok,
-                idopontok,
-                kedvezmeny,
-                klima,
-                helyjegy,
-                kepek
-            }),
-        });
+//         const kep1 = document.querySelector("#kep1").value;
+//         const kep2 = document.querySelector("#kep2").value;
 
-        const vissza = await response.json();
+//         console.log({
+//                 jarat,
+//                 induloallomas,
+//                 celallomas,
+//                 allomasok,
+//                 idopontok,
+//                 kedvezmeny,
+//                 klima,
+//                 helyjegy,
+//                 kortablak,
+//                 idotartam,
+//                 kep1,
+//                 kep2,
+//             });
+        
 
-        if (response.ok) {
-            alert(vissza.msg);
-            window.location.href = '/api/schedules-backend';
-        } else {
-            alert(vissza.msg);
-        }
+//         // Küldés backend felé
+//         // const response = await fetch(`/api/new-schedule`, {
+//         //     method: 'POST',
+//         //     headers: { 'Content-Type': 'application/json' },
+//         //     body: JSON.stringify({
+//         //         jarat,
+//         //         induloallomas,
+//         //         celallomas,
+//         //         allomasok,
+//         //         idopontok,
+//         //         kedvezmeny,
+//         //         klima,
+//         //         helyjegy,
+//         //         kortablak,
+//         //         idotartam,
+//         //         kep1,
+//         //         kep2,
+//         //     }),
+//         // });
 
-    } catch (error) {
-        console.error('Hiba:', error.message);
-    }
-}
+//         const vissza = await response.json();
+
+//         if (response.ok) {
+//             alert(vissza.msg);
+//             window.location.href = '/api/schedules-backend';
+//         } else {
+//             alert(vissza.msg);
+//         }
+
+//     } catch (error) {
+//         console.error('Hiba:', error.message);
+//     }
+// }
